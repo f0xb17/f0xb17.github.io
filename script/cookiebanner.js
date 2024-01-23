d0c8587af64d58157a74ab22efdd2c2396d7972c
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-01-23 20:27:53 +0100

    Fixed CookieBanner Logic
    Added new CookieBanner Logic

---
+++ script/cookiebanner.js
@@ -0,0 +1,21 @@
+/**
+ * This is the entire logic for the cookie banner. 
+ * Can be extended if necessary, but since I have a no-track policy, 
+ * nothing more is done here than telling the user exactly that and then hiding again. 
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cookieBanner = document.querySelector(".cookie-banner")
+    const cookieBtn = document.querySelector(".cookie-btn")
+
+    cookieBtn.addEventListener('click', () => {
+        cookieBanner.style.display = 'none'
+        localStorage.setItem('cookieBannerDisplayed', true)
+    })
+
+    window.addEventListener('load', () => {
+        if(!localStorage.getItem('cookieBannerDisplayed')) {
+            cookieBanner.style.display = 'flex'
+        }
+    })
+})
--- script/mobile.js
+++ script/mobile.js
@@ -1,21 +1,12 @@
-const mobileMenu = document.querySelector("#mobile-menu")
-const cookieBanner = document.querySelector(".cookie-banner")
-const cookieBtn = document.querySelector("#cookie-btn")
 
-mobileMenu.addEventListener("change", function() {
+/**
+ * This method serves as a temporary fix for mobile browsers to prevent any overflow.
+ */
+
+const mobileMenu = document.querySelector('#mobile-menu')
+
+mobileMenu.addEventListener('change', function() {
     if(this.checked) {
-        document.body.style.overflow = "hidden"
-        cookieBanner.style.display = "none"
-    }
-})
-
-cookieBtn.addEventListener("click", function() {
-    cookieBanner.style.display = "none"
-})
-
-window.addEventListener("load", function() {
-    if(!localStorage.getItem("cookieBannerDisplayed")) {
-        cookieBanner.style.display = "block"
-        localStorage.setItem("cookieBannerDisplayed", true)
+        document.body.style.overflow = 'hidden'
     }
 })
