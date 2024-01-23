dc9db701454eaaae8cc191fb512c4eeceb38a785
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-01-23 16:14:14 +0100

    Change some Minor Things
    Added Cookie Banner

--- script/mobile.js
+++ script/mobile.js
@@ -1,7 +1,21 @@
-var hamburg = document.getElementById("mobile-menu")
+const mobileMenu = document.querySelector("#mobile-menu")
+const cookieBanner = document.querySelector(".cookie-banner")
+const cookieBtn = document.querySelector("#cookie-btn")
 
-hamburg.addEventListener("change", function() {
+mobileMenu.addEventListener("change", function() {
     if(this.checked) {
         document.body.style.overflow = "hidden"
+        cookieBanner.style.display = "none"
+    }
+})
+
+cookieBtn.addEventListener("click", function() {
+    cookieBanner.style.display = "none"
+})
+
+window.addEventListener("load", function() {
+    if(!localStorage.getItem("cookieBannerDisplayed")) {
+        cookieBanner.style.display = "block"
+        localStorage.setItem("cookieBannerDisplayed", true)
     }
 })
